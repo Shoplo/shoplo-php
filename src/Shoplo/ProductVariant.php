4146d727a719dc9481faea9123c81457f89e7c3f
Mateusz Sojda <mateusz@sojda.pl>
2013-07-01 13:40:11 +0200

    Added "retrieve" method to product variants

--- src/Shoplo/ProductVariant.php
+++ src/Shoplo/ProductVariant.php
@@ -4,4 +4,21 @@ namespace Shoplo;
 
 class ProductVariant extends Resource
 {
+    public function retrieve($productId, $variantId = 0, $params = array(), $cache = false)
+    {
+        if ($variantId == 0) {
+            if (!$cache || !isset($this->bucket['variant'])) {
+                $params                  = $this->prepare_params($params);
+                $result                  = empty($params) ? $this->send($this->prefix . "/variants/" . $productId) : $this->send($this->prefix . "/variants/" . $productId ."?". $params);
+                $this->bucket['variant'] = $this->prepare_result($result);
+            }
+            return $this->bucket['variant'];
+        } else {
+            if (!$cache || !isset($this->bucket['variant'][$variantId])) {
+                $result                       = $this->send($this->prefix . "/products/" . $productId . "/variants/" . $variantId);
+                $this->bucket['variant'][$variantId] = $this->prepare_result($result);
+            }
+            return $this->bucket['variant'][$variantId];
+        }
+    }
 }
