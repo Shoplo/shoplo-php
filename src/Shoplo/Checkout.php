4f05fd0759bf51dc4f8e8805c713b394f81d751b
Mateusz Sojda <mateusz@sojda.pl>
2013-08-22 15:30:48 +0200

    Added single checkout retrieve method (by user_key)

--- src/Shoplo/Checkout.php
+++ src/Shoplo/Checkout.php
@@ -4,14 +4,25 @@ namespace Shoplo;
 
 class Checkout extends Resource
 {
-    public function retrieve($params = array(), $cache = false)
+    public function retrieve($userKey = null, $params = array(), $cache = false)
     {
-        if (!$cache || !isset($this->bucket['carts'])) {
-            $params                = $this->prepare_params($params);
-            $result                = empty($params) ? $this->send($this->prefix . "checkout") : $this->send($this->prefix . "checkout?" . $params);
-            $this->bucket['carts'] = $this->prepare_result($result);
+        if( is_null($userKey) )
+        {
+            if (!$cache || !isset($this->bucket['carts'])) {
+                $params                = $this->prepare_params($params);
+                $result                = empty($params) ? $this->send($this->prefix . "checkout") : $this->send($this->prefix . "checkout?" . $params);
+                $this->bucket['carts'] = $this->prepare_result($result);
+            }
+            return $this->bucket['carts'];
         }
-        return $this->bucket['carts'];
+        else
+        {
+            if (!$cache || !isset($this->bucket['carts'][$userKey])) {
+                $result                            = $this->send($this->prefix . "/checkout/" . $userKey);
+                $this->bucket['carts'][$userKey] = $this->prepare_result($result);
+            }
+            return $this->bucket['carts'][$userKey];
+        }
     }
 
     public function count($params = array())
